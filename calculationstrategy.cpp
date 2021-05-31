cd0a26bff153261858ca74bb543d561b6f44e20e
Anton <inamometr1@gmail.com>
2021-05-31 16:59:45 +0700

    Fix:CalculationStrategy

--- calculationstrategy.cpp
+++ calculationstrategy.cpp
@@ -1,6 +1,16 @@
 #include "calculationstrategy.h"
 
-CalculationStrategy::CalculationStrategy()
+double CalculationStrategy::FullDirSize(QString Dir)
 {
-
+    double DirectorySize = 0;
+    QDir directory(Dir);
+    QFileInfoList entryList = directory.entryInfoList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot);
+    for(auto &currentFile : entryList)
+    {
+        if(currentFile.isDir())
+            DirectorySize += FullDirSize(Dir + '/' +currentFile.fileName());
+        else
+            DirectorySize += currentFile.size();
+    }
+    return DirectorySize;
 }
--- calculationstrategy.h
+++ calculationstrategy.h
@@ -1,11 +1,19 @@
 #ifndef CALCULATIONSTRATEGY_H
 #define CALCULATIONSTRATEGY_H
 
+#include <QString>
+#include <QDir>
+#include <QList>
+#include <iostream>
+
 
 class CalculationStrategy
 {
 public:
-    CalculationStrategy();
+    virtual QList<std::pair<QString,double>> CalculationMethod(const QString&) = 0;
+    static double FullDirSize(QString Dir);
+
+friend void PrintInfo(QString path, CalculationStrategy* strat);
 };
 
 #endif // CALCULATIONSTRATEGY_H
